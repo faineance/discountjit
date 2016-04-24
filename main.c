94c015204447830af5cf902dbf214245867f525d
faineance <faineance@users.noreply.github.com>
2016-04-24 20:57:33 +0100

    fix autoformat

--- main.c
+++ main.c
@@ -7,23 +7,26 @@
 
 #include "sys/mman.h"
 
+
+
+
 int main() {
 
     void *code = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
+
+    int curr_offset = 0;
+
     int v = 0;
 
-    memcpy(code, && inc_start, &&inc_end - && inc_start);
+    memcpy(code, &&inc_start, &&inc_end - &&inc_start);
 
-    goto
-    *code;
+    goto *code;
 
     inc_start:
     v += 1;
     v += 1;
-    void *p_end =&&end;
-    goto
-    *p_end;
+    void *p_end =&&end; goto *p_end;
     inc_end:;
     end:
 

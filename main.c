65e07d2a0fa84613437e9971687b3c6e3ec2afb2
faineance <faineance@users.noreply.github.com>
2016-04-25 09:00:24 +0100

    moar

--- main.c
+++ main.c
@@ -7,31 +7,38 @@
 
 #include "sys/mman.h"
 
-
-
-
 int main() {
 
     void *code = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE , MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
+    void *p_end = &&end;
 
     int v = 0;
 
+    size_t offset = 0;
     memcpy(code, &&inc_start, &&inc_end - &&inc_start);
+    offset += &&inc_end - &&inc_start;
+    memcpy(code + offset, &&end_start, &&end_end - &&end_start);
+    offset += &&end_end - &&end_start;
+
 
     mprotect(code, PAGE_SIZE, PROT_READ | PROT_EXEC); // hah safety
     goto *code;
 
     inc_start:
-    v += 1;
-    v += 1;
-    void *p_end =&&end; goto *p_end;
+        v += 1;
+        v += 1;
     inc_end:;
+
+
+    end_start:
+    goto *p_end;
+    end_end:;
+
     end:
 
-
     munmap(code, PAGE_SIZE);
-    printf("%d", v);
+    printf("%d\n", v);
     return 0;
 }
 
